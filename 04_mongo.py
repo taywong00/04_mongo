57f852bdda008c064a941dd6bbc45045f4979219
Charles <cweng@stuy.edu>
2018-02-15 13:14:51 -0500

    stat of next assignment

--- 04_mongo.py
+++ 04_mongo.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 
-# Charles Weng, Taylor Wong# SoftDev2 pd7
+# Charles Weng, Taylor Wong
+# SoftDev2 pd7
 # K #04: Mi only nyam ital food, mon!
 # 2018-02-15
 
@@ -12,15 +13,17 @@ from pymongo import MongoClient
 '''
 
 c = MongoClient('lisa.stuy.edu', 27017)
-mfDB = c['test']
-r = mfDB['restaurants']
+test = c['test']
+r = test['restaurants']
 
 
 '''
 ================================================================================
                                  functions
+    test.returant parsing functions
 ================================================================================
 '''
+
 
 # search by borough
 def find_b(b):
@@ -48,28 +51,75 @@ def find_zg(z, g):
 
 # search by zipcode and score
 def find_zs(z, s):
-    d = r.find({'$and':[{"address.zipcode":str(z)}, {"grades.score": {'$lt': s}}]})
+    d = r.find({'$and': [{"address.zipcode": str(z)}, {"grades.score": {'$lt': s}}]})
     for doc in d:
         print doc
     return d
+
 
 # search by zipcode and cuisine type
 def find_a(z, c):
-    d = r.find({'$and':[{"address.zipcode":str(z)}, {"cuisine": cuisine}]})
+    d = r.find({'$and': [{"address.zipcode": str(z)}, {"cuisine": c}]})
     for doc in d:
         print doc
     return d
 
 
+
+
+
 '''
 ================================================================================
-                                      tests
+                                set up & functions 2
 ================================================================================
 '''
 
-# find_b('Manhattan') # very big
-find_z('10282')
-find_zg('10282', 'A')
-find_zs('10282', '11')
-find_a('10282', 'Chinese'):
-                                                
+db = c['+X米X+']
+p = db['pokedex']
+
+# does data already exist
+if (p.length == 0):
+    pass
+
+
+# searches by type
+def find_t(t):
+    pass
+
+
+# searches by weaknesses
+def find_w(w):
+    pass
+
+
+# searches by type and weaknesses
+def find_tw(t, w):
+    pass
+
+
+# searches by spawn time
+def find_s(s):
+    pass
+
+
+'''
+================================================================================
+tests
+================================================================================
+'''
+
+# restaurant tests
+if(False):
+    find_b('Manhattan')  # very big
+    find_z('10282')
+    find_zg('10282', 'A')
+    find_zs('10282', '11')
+    find_a('10282', 'Chinese')
+
+
+# pokedex tests
+if(True):
+    find_t('water')
+    find_w('fire')
+    find_tw('grass', 'fighting')
+    find_s('11:30')
